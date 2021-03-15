a4c3b584cd3addc9907f9b99563e6f89f69a6440
Kubvv <Jakub.grondziowski@gmail.com>
2021-03-15 14:56:39 +0100

    Firebase setup and bundle
    + Added bundle to save texts after leaving app
    + Added basic firebase config stuff
    +- Fixed status bar color and login password color

--- Tasqr/app/src/main/java/com/example/tasqr/LoginActivity.java
+++ Tasqr/app/src/main/java/com/example/tasqr/LoginActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button registerButton;
+    private EditText etMail;
+    private EditText etPass;
+    Bundle bundle = new Bundle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +23,24 @@ public class LoginActivity extends AppCompatActivity {
 
         registerButton = (Button) findViewById(R.id.register_activity_button);
         registerButton.setOnClickListener(v -> openRegisterActivity());
+        etMail = (EditText) findViewById(R.id.email_textfield);
+        etPass = (EditText) findViewById(R.id.password_textfield);
     }
 
     public void openRegisterActivity() {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    protected void onResume() {
+        etMail.setText(bundle.getString("etMail"));
+        etPass.setText(bundle.getString("etPass"));
+        super.onResume();
+    }
+
+    protected void onPause() {
+        bundle.putString("etMail", etMail.getText().toString());
+        bundle.putString("etPass", etPass.getText().toString());
+        super.onPause();
     }
 }
--- Tasqr/app/src/main/java/com/example/tasqr/RegisterActivity.java
+++ Tasqr/app/src/main/java/com/example/tasqr/RegisterActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Button loginButton;
+    private EditText ets[] = new EditText[4];
+    private Bundle bundle = new Bundle();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +21,29 @@ public class RegisterActivity extends AppCompatActivity {
 
         loginButton = (Button) findViewById(R.id.login_activity_button);
         loginButton.setOnClickListener(v -> openLoginActivity());
+        ets[0] = findViewById(R.id.name_textfield);
+        ets[1] = findViewById(R.id.surname_textfield);
+        ets[2] = findViewById(R.id.email_textfield);
+        ets[3] = findViewById(R.id.password_textfield);
     }
 
     public void openLoginActivity() {
         RegisterActivity.this.finish();
     }
 
+    protected void onResume() {
+        ets[0].setText(bundle.getString("etName"));
+        ets[1].setText(bundle.getString("etSurname"));
+        ets[2].setText(bundle.getString("etMail"));
+        ets[3].setText(bundle.getString("etPass"));
+        super.onResume();
+    }
+
+    protected void onPause() {
+        bundle.putString("etName", ets[0].getText().toString());
+        bundle.putString("etSurname", ets[1].getText().toString());
+        bundle.putString("etMail", ets[2].getText().toString());
+        bundle.putString("etPass", ets[3].getText().toString());
+        super.onPause();
+    }
 }
