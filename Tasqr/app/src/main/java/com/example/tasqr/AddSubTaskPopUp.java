e28a5579e5d04d3e83323f7657dc2e3216484f51
SCube19 <kondorcs@wp.pl>
2021-03-25 01:11:27 +0100

    subtask popup scratch

---
+++ Tasqr/app/src/main/java/com/example/tasqr/AddSubTaskPopUp.java
@@ -0,0 +1,44 @@
+package com.example.tasqr;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class AddSubTaskPopUp extends AppCompatDialogFragment {
+
+    private EditText subTaskName;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.popup_addsubtask, null);
+
+        subTaskName = view.findViewById(R.id.subtask_name);
+
+        builder.setView(view).setTitle("Add Sub Task")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setPositiveButton("confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        return builder.create();
+    }
+}
